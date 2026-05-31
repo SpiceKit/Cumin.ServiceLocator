f1676589f470d8b5c65233962d8e2acb1a17ae86
G.Takakura <gixa.aturakka+gitlab@gmail.com>
2026-05-31 18:29:20 +0900

    Pure C#に変更

---
+++ Cumin.ServiceLocator/Sample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public interface ITest
+{
+	void Test();
+}
+public class Sample : ITest
+{
+	public void Test()
+	{
+		Console.WriteLine("Test");
+	}
+}
